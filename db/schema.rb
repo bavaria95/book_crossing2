035c967d0d6391ed9dd3e28a4009d96dd347b0b6
Dima Boyko <dimka.boyko@hotmail.com>
2015-11-20 19:34:25 +0100

    scaffold :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120175249) do
+ActiveRecord::Schema.define(version: 20151120181108) do
 
   create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
